792ec81728d8d4812043e8d7dd54d74a3de03112
Kyle Terry <kyle@kyleterry.com>
2019-05-14 15:18:01 -0700

    adds workflow model helper and a test for listing workflows

--- pkg/client/client_test.go
+++ pkg/client/client_test.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"context"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -15,6 +16,20 @@ import (
 	"github.com/puppetlabs/nebula/pkg/config"
 	"github.com/stretchr/testify/require"
 )
+
+func makeWorkflowFixture(id, repository, branch, path string) *models.Workflow {
+	createdAt := time.Now().String()
+	updatedAt := time.Now().String()
+
+	return &models.Workflow{
+		ID:         &id,
+		Repository: &repository,
+		Branch:     &branch,
+		Path:       &path,
+		CreatedAt:  &createdAt,
+		UpdatedAt:  &updatedAt,
+	}
+}
 
 func withAPIClient(t *testing.T, routes http.Handler, fn func(c *APIClient)) {
 	tmpdir, err := ioutil.TempDir("", "nebula-cli-test")
@@ -61,28 +76,37 @@ func TestLogin(t *testing.T) {
 }
 
 func TestWorkflowCreate(t *testing.T) {
-	id := "some-workflow-id"
-	repository := "test-repo"
-	branch := "test-branch"
-	path := "workflow.yaml"
-	createdAt := time.Now().String()
-	updatedAt := time.Now().String()
-
-	wfm := &models.Workflow{
-		ID:         &id,
-		Repository: &repository,
-		Branch:     &branch,
-		Path:       &path,
-		CreatedAt:  &createdAt,
-		UpdatedAt:  &updatedAt,
-	}
+	wfm := makeWorkflowFixture("id1234", "repo1", "branch1", "workflow.yaml")
 
 	routes := &testutil.MockRoutes{}
 	routes.Add("/api/workflows", http.StatusCreated, wfm, nil)
 
 	withAPIClient(t, routes, func(c *APIClient) {
-		wf, err := c.CreateWorkflow(context.Background(), "test-repo", "test-branch", "workflow.yaml")
+		wf, err := c.CreateWorkflow(context.Background(), "repo1", "branch1", "workflow.yaml")
 		require.NoError(t, err, "could not create workflow")
-		require.Equal(t, *wf.ID, id)
+		require.Equal(t, *wf.ID, "id1234")
+		require.Equal(t, *wf.Repository, "repo1")
+	})
+}
+
+func TestWorkflowList(t *testing.T) {
+	wfl := &models.Workflows{}
+
+	for i := 0; i < 10; i++ {
+		wfm := makeWorkflowFixture(fmt.Sprintf("id-%d", i), "repo", "branch", fmt.Sprintf("workflow-%d.yaml", i))
+		wfl.Items = append(wfl.Items, wfm)
+	}
+
+	routes := &testutil.MockRoutes{}
+	routes.Add("/api/workflows", http.StatusOK, wfl, nil)
+
+	withAPIClient(t, routes, func(c *APIClient) {
+		wfl, err := c.ListWorkflows(context.Background())
+		require.NoError(t, err, "could not list workflows")
+
+		for i := 0; i < 10; i++ {
+			wf := wfl.Items[i]
+			require.Equal(t, *wf.ID, fmt.Sprintf("id-%d", i))
+		}
 	})
 }
