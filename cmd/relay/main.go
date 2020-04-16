2175e0d74e9b767fa5e77fb1707fa80ca50470e8
Noah Muldavin <nmuldavin@gmail.com>
2020-04-16 15:51:33 -0700

    Restore 3.0 cli commands

--- cmd/relay/main.go
+++ cmd/relay/main.go
@@ -1,30 +1,18 @@
 package main
 
 import (
-	"os"
+	"log"
 
-	"github.com/spf13/cobra"
+	"github.com/puppetlabs/relay/pkg/cmd"
 )
 
 func main() {
-	cmd := &cobra.Command{
-		Use:   "relay",
-		Short: "Relay by Puppet.",
-		Args:  cobra.MinimumNArgs(1),
-		Long: `Relay connects your tools, APIs, and infrastructure 
-to automate common tasks through simple event driven workflows.`,
+	command, err := cmd.NewRootCommand()
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	cmd.PersistentFlags().BoolP("verbose", "v", false, "print verbose output")
-	cmd.PersistentFlags().BoolP("debug", "d", false, "print debugging information")
-	cmd.PersistentFlags().StringP("out", "o", "text", "output type: (text|json)")
-	// Config flag is hidden for now
-	cmd.PersistentFlags().StringP("config", "c", "", "path to config file (default is $HOME.config/relay)")
-	cmd.PersistentFlags().MarkHidden("config")
-
-	cmd.AddCommand(NewAuthCommand())
-
-	if err := cmd.Execute(); err != nil {
-		os.Exit(1)
+	if err := command.Execute(); err != nil {
+		log.Fatal(err)
 	}
 }
