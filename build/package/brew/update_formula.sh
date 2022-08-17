9bc783bd64981d9b11a6e6c2639a824cedbbdfdd
Rick Lane <rick.lane@puppet.com>
2022-08-17 09:43:29 -0700

    Use main branch instead of master

--- build/package/brew/update_formula.sh
+++ build/package/brew/update_formula.sh
@@ -2,7 +2,7 @@
 GITHUB_TOKEN=$(ni get -p {.github_token})
 TAG=$(ni get -p {.tag})
 SHA=$(ni get -p {.sha})
-if [[ $GITHUB_TOKEN =~ .{25,} ]] && [[ $TAG =~ ^v ]] && [[ $SHA =~ .{63,} ]] ; then 
+if [[ $GITHUB_TOKEN =~ .{25,} ]] && [[ $TAG =~ ^v ]] && [[ $SHA =~ .{63,} ]] ; then
   git clone https://${GITHUB_TOKEN}@github.com/puppetlabs/homebrew-puppet
   cd homebrew-puppet
   git config user.name "Relay Autobot" && git config user.email "relay@users.noreply.github.com"
@@ -15,8 +15,8 @@ if [[ $GITHUB_TOKEN =~ .{25,} ]] && [[ $TAG =~ ^v ]] && [[ $SHA =~ .{63,} ]] ; t
   git push origin ${PUBLISH_BRANCH}
   PULLS_URI="https://api.github.com/repos/puppetlabs/homebrew-puppet/pulls"
   AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
-  NEW_PR_RESP=$(curl --data "{\"title\": \"${COMMIT_MESSAGE}\", \"head\": \"${PUBLISH_BRANCH}\", \"base\": \"master\"}" -X POST -s -H "${AUTH_HEADER}" ${PULLS_URI})
-  if [[ $? == 0 ]]; then 
+  NEW_PR_RESP=$(curl --data "{\"title\": \"${COMMIT_MESSAGE}\", \"head\": \"${PUBLISH_BRANCH}\", \"base\": \"main\"}" -X POST -s -H "${AUTH_HEADER}" ${PULLS_URI})
+  if [[ $? == 0 ]]; then
     PR_URL=$(echo $NEW_PR_RESP | jq ._links.html.href)
   ni output set --key result --value "Success! Pull request for $TAG submitted at: $PR_URL"
   else
